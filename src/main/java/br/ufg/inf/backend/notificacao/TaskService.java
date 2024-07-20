f4ec6c5a08c118b861a255d165842786120828e1
Sergio Cabral <scastrocabral@gmail.com>
2024-07-19 21:29:58 -0300

    Implementa doPut e doDelete com injeção de dependnecias

--- src/main/java/br/ufg/inf/backend/notificacao/TaskService.java
+++ src/main/java/br/ufg/inf/backend/notificacao/TaskService.java
@@ -7,60 +7,85 @@ import br.ufg.inf.backend.tasks.model.Task;
 import br.ufg.inf.backend.tasks.model.TaskPriority;
 
 public class TaskService {
-	
-	
 
 	private static List<Task> tasksList = new ArrayList<>();
 
 	private NotificacaoService notificacaoService;
-	
+
 	public TaskService() {
 	}
-	
+
 	public TaskService(NotificacaoService notificacaoService) {
 		this.notificacaoService = notificacaoService;
 	}
-	
-	
+
 	public String adicionar(String myTask, String myPriority) {
 		Task newTask = new Task(myTask, searchPriority(myPriority));
-		
+
 		for (Task task : tasksList) {
 			if (task.getTaskName().equals(newTask.getTaskName())) {
 				return "Estah tarefa ja existe!";
 			}
 		}
-		
+
 		tasksList.add(newTask);
-		
-		return notificacaoService.enviarNotificacao("adicionar");
+
+		return "Tarefa adicionada com sucesso!\n" + notificacaoService.enviarNotificacao("adicionar");
 	}
-	
-	public void atualizar() {
-		
+
+	public String atualizar(String newTask, String newPriority, int indice) {
+		for (Task task : tasksList) {
+			if (tasksList.indexOf(task) == indice - 1) {
+				task.setTaskName(newTask);
+				task.setTaskPriority(searchPriority(newPriority));
+				return "Tarefa atualizada com sucesso!\n" + notificacaoService.enviarNotificacao("atualizar");
+			}
+		}
+
+		return String.format("Tarefa nao encontrada. Sua lista possui apenas %d tarefa(s) cadastrada(s)",
+				tasksList.size(), indice);
 	}
-	
-	public void remover() {
+
+	public String remover(String myTask) {
 		
+		if (listIsEmpty()) {
+			return "Sua lista de tarefas estah vazia!";
+		} else {
+			for (Task task : tasksList) {
+				if (task.getTaskName().equals(myTask)) {
+					tasksList.remove(tasksList.indexOf(task));
+					return String.format("Tarefa %s removida com sucesso!", myTask);
+				} 
+			}
+			return "Voce nao possui essa tarefa cadastrada na sua lista!";
+		}
 	}
-	
+
 	public String listar() {
 		if (tasksList.size() == 0) {
 			return "Você não possui tarefas!";
 		}
 		String myTasks = "";
 		for (Task task : tasksList) {
-			myTasks += tasksList.indexOf(task) + 1 + ": " + task.getTaskName() + " - Prioridade: " + task.getTaskPriority() + "\n";
+			myTasks += tasksList.indexOf(task) + 1 + ": " + task.getTaskName() + " - Prioridade: "
+					+ task.getTaskPriority() + "\n";
 		}
 		return myTasks;
 	}
-	
+
 	private TaskPriority searchPriority(String myPriority) {
-		try { 
+		try {
 			return TaskPriority.getPriority(myPriority);
 		} catch (IllegalArgumentException e) {
 			throw new RuntimeException("Prioridade informada invalida!! Informe entre: alta, media ou baixa");
 		}
-		
+
+	}
+	
+	private Boolean listIsEmpty() {
+		if (tasksList.isEmpty()) {
+			return true;
+		}
+		return false;
 	}
 }
--- src/main/java/br/ufg/inf/backend/notificacao/TaskServlet.java
+++ src/main/java/br/ufg/inf/backend/notificacao/TaskServlet.java
@@ -18,86 +18,48 @@ public class TaskServlet extends HttpServlet {
 
 	private String myNewTask;
 	private String myNewPriority;
-	
-	private TaskService taskService = new TaskService();
-	private TaskServiceWithEmailNotification taskWithEmailNotification = new  TaskServiceWithEmailNotification(new EmailNotificacaoService());
-	private TaskServiceWithSMSNotification taskWithSMSNotification = new TaskServiceWithSMSNotification(new SMSNotificacaoService());
 
-	
+	private TaskService taskService = new TaskService();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
 			resp.getWriter().println(taskService.listar());
-		} catch(Exception e) {
+		} catch (Exception e) {
 			resp.getWriter().print("Algo deu errado");
 		}
 	}
 
-//	@Override
-//	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		int indice;
-//
-//		myNewTask = req.getParameter("task");
-//		myNewPriority = req.getParameter("priority");
-//		try {
-//			indice = Integer.parseInt(req.getParameter("index"));
-//		} catch (NumberFormatException e) {
-//			throw new RuntimeException("Valor para o indice invalido!");
-//		}
-//
-//		if (listIsEmpty(resp)) {
-//			return;
-//		} else {
-//
-//			for (Task task : tasksList) {
-//				if (tasksList.indexOf(task) == indice - 1) {
-//					task.setTaskName(myNewTask);
-//					task.setTaskPriority(searchPriority(myNewPriority));
-//					resp.getWriter().append("Tarefa atualizada com sucesso!!");
-//					return;
-//				}
-//			}
-//
-//			resp.getWriter().append(String.format("Tarefa nao encontrada. Sua lista possui apenas %d tarefa(s) cadastrada(s)", tasksList.size()));
-//		}
-//
-//	}
-//
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		taskService = taskWithSMSNotification;
-		
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int indice;
+		taskService = new TaskServiceWithEmailNotification(new EmailNotificacaoService());
+
 		myNewTask = req.getParameter("task");
 		myNewPriority = req.getParameter("priority");
+		try {
+			indice = Integer.parseInt(req.getParameter("index"));
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("Valor para o indice invalido!");
+		}
 		
+		resp.getWriter().println(taskService.atualizar(myNewTask, myNewPriority, indice));
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		taskService = new TaskServiceWithSMSNotification(new SMSNotificacaoService());
+
+		myNewTask = req.getParameter("task");
+		myNewPriority = req.getParameter("priority");
+
 		resp.getWriter().println(taskService.adicionar(myNewTask, myNewPriority));
 	}
-//
-//	@Override
-//	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		myNewTask = req.getParameter("task");
-//
-//		if (listIsEmpty(resp)) {
-//			return;
-//		} else {
-//			for (Task task : tasksList) {
-//				if (task.getTaskName().equals(myNewTask)) {
-//					tasksList.remove(tasksList.indexOf(task));
-//					resp.getWriter().append(String.format("Tarefa %s removida com sucesso!", myNewTask));
-//				} else {
-//					resp.getWriter().append("Voce nao possui essa tarefa na sua lista! Nao eh possivel remover");
-//				}
-//			}
-//		}
-//	}
-//
-//	private Boolean listIsEmpty(HttpServletResponse resp) throws IOException {
-//		if (tasksList.isEmpty()) {
-//			resp.getWriter().append("Sua lista de tarefas estah vazia!");
-//			return true;
-//		}
-//		return false;
-//	}
-//
-	
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String myTask = req.getParameter("task");
+		
+		resp.getWriter().println(taskService.remover(myTask));
+	}
 }
